86d0c290191b1b537a1e0fe98dd9dfcd926d5a0c
mariiamel <mariiamelnikova96@gmail.com>
2021-03-10 09:52:12 -0800

    finished hw

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Stopwatch from './components/Stopwatch.jsx'
+import './App.css'
+
+
+export default class App extends Component {
+  render() {
+    return(
+      <div>
+        <Stopwatch />
+      </div>
+    )
+  }
+}
