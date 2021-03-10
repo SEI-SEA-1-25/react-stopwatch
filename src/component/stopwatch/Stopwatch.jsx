0a9cbf7724a8a83beef807970fcfe48120c50278
kchu01 <chukathy01@gmail.com>
2021-03-09 21:39:45 -0800

    Include handleStart event

--- src/component/stopwatch/Stopwatch.jsx
+++ src/component/stopwatch/Stopwatch.jsx
@@ -1,6 +1,32 @@
 import { Component } from 'react'
 
 export default class Stopwatch extends Component {
+    constructor() {
+        super()
+        this.state = {
+            counter: 0,
+            interval: null
+        }
+    }
+    // event handlers
+    handleReset = (e) => {
+
+
+        console.log('reset works')
+    }
+
+    handleStart = (e) => {
+        if (this.state.interval) {
+            return
+        } this.setState({
+            interval: setInterval(this.incrementTime, 1000)
+        }, () => console.log('rev up those fryers'))
+    }
+
+    handlePause = (e) => {
+        console.log('pause')
+    }
+
     render() {
         return (
             <div>
