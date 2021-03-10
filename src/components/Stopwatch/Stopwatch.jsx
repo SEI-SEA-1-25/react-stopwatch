b6280f60106ba4d7037ed7f30076175bdfab6769
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-09 18:46:17 -0800

    Initial Compnent

---
+++ src/components/Stopwatch/Stopwatch.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react' 
+
+export default class Stopwatch extends Component {
+    render () {
+        return (
+            <div>
+                <h1></h1>
+                <div>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        )
+    }
+}
